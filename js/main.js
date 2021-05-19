762fe047b92b0aa5e5eb4bec2ea597c2148a836b
Russell-Adams <76688591+Russell-Adams@users.noreply.github.com>
2021-05-20 01:22:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper('.channel-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 5.75,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next',
+      prevEl: '.channel-button-prev',
+    },
+});
+var swiper = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 2.88,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+});
