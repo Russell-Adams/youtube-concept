06c08c4fdbfe0702f5820e1b2cfd3d82f62e0504
Russell-Adams <76688591+Russell-Adams@users.noreply.github.com>
2021-05-20 20:08:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,86 @@
-var swiper = new Swiper('.channel-slider', {
+const swiperA = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 5.75,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300: {
+        slidesPerView: 4
+      },
+      1100: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next',
       prevEl: '.channel-button-prev',
     },
 });
-var swiper = new Swiper('.recommended-slider', {
+const swiperB = new Swiper('.recommended-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 2.88,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3
+    },
+    1100: {
+      slidesPerView: 2
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.recommended-button-next',
     prevEl: '.recommended-button-prev',
   },
 });
+const swiperC = new Swiper('.recommended-2-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300: {
+        slidesPerView: 4
+      },
+      1100: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      },
+    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-2-button-next',
+    prevEl: '.recommended-2-button-prev',
+  },
+});
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiperA.destroy();
+  swiperB.destroy();
+  swiperC.destroy();
+}
